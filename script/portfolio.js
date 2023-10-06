22fe22e148be4c5422390abb22f2de34f0cbf94c
entia <entia1978@gmail.com>
2023-10-06 18:01:24 +0900

    1006class

--- script/portfolio.js
+++ script/portfolio.js
@@ -1,96 +1,124 @@
 
+// 핵심역량 토글
+$(".keypoint").click(function(){
+    $(".pop_keypoint").toggle();
+});
+
+
+
 // 디테일콘텐츠 스몰이미지 클릭하면 팝업이미지 띄우기
 $('.detail1').on('click',function(e){
     e.preventDefault();
     $('.contents1').show();
+    $('.close1').show();
+    $.fn.fullpage.setAllowScrolling(false) //풀페이지 스크롤 해제
 })
 $('.detail2').on('click',function(e){
     e.preventDefault();
     $('.contents2').show();
+    $('.close2').show();
+    $.fn.fullpage.setAllowScrolling(false) //풀페이지 스크롤 해제
 })
 $('.detail3').on('click',function(e){
     e.preventDefault();
     $('.contents3').show();
+    $('.close3').show();
+    $.fn.fullpage.setAllowScrolling(false) //풀페이지 스크롤 해제
 })
 $('.detail4').on('click',function(e){
     e.preventDefault();
     $('.contents4').show();
+    $('.close4').show();
+    $.fn.fullpage.setAllowScrolling(false) //풀페이지 스크롤 해제
 })
 
 
 // 클로즈버튼 누르면 팝업닫기
 $('.close1').on('click',function(){
     $('.contents1').hide();
+    $('.close1').hide();
+    $.fn.fullpage.setAllowScrolling(true) //풀페이지 스크롤 재개
 })
 $('.close2').on('click',function(){
     $('.contents2').hide();
+    $('.close2').hide();
+    $.fn.fullpage.setAllowScrolling(true) //풀페이지 스크롤 재개
 })
 $('.close3').on('click',function(){
     $('.contents3').hide();
+    $('.close3').hide();
+    $.fn.fullpage.setAllowScrolling(true) //풀페이지 스크롤 재개
 })
 $('.close4').on('click',function(){
     $('.contents4').hide();
+    $('.close4').hide();
+    $.fn.fullpage.setAllowScrolling(true) //풀페이지 스크롤 재개
 })
 
-// sns콘텐츠 스몰이미지 클릭하면 팝업이미지 띄우기
-$('.sns1').on('click',function(e){
-    e.preventDefault();
-    $('.sns_popup1').show();
-})
-$('.sns2').on('click',function(e){
-    e.preventDefault();
-    $('.sns_popup2').show();
-})
-$('.sns3').on('click',function(e){
-    e.preventDefault();
-    $('.sns_popup3').show();
-})
-$('.sns4').on('click',function(e){
-    e.preventDefault();
-    $('.sns_popup4').show();
-})
-$('.sns5').on('click',function(e){
-    e.preventDefault();
-    $('.sns_popup5').show();
-})
-$('.sns6').on('click',function(e){
-    e.preventDefault();
-    $('.sns_popup6').show();
-})
-$('.sns7').on('click',function(e){
-    e.preventDefault();
-    $('.sns_popup7').show();
-})
-$('.sns8').on('click',function(e){
-    e.preventDefault();
-    $('.sns_popup8').show();
-})
 
-// 클로즈버튼 누르면 팝업닫기
-$('.sns_close1').on('click',function(){
-    $('.sns_popup1').hide();
-})
-$('.sns_close2').on('click',function(){
-    $('.sns_popup2').hide();
-})
-$('.sns_close3').on('click',function(){
-    $('.sns_popup3').hide();
-})
-$('.sns_close4').on('click',function(){
-    $('.sns_popup4').hide();
-})
-$('.sns_close5').on('click',function(){
-    $('.sns_popup5').hide();
-})
-$('.sns_close6').on('click',function(){
-    $('.sns_popup6').hide();
-})
-$('.sns_close7').on('click',function(){
-    $('.sns_popup7').hide();
-})
-$('.sns_close8').on('click',function(){
-    $('.sns_popup8').hide();
-})
+
+
+
+
+// // sns콘텐츠 스몰이미지 클릭하면 팝업이미지 띄우기
+// $('.sns1').on('click',function(e){
+//     e.preventDefault();
+//     $('.sns_popup1').show();
+// })
+// $('.sns2').on('click',function(e){
+//     e.preventDefault();
+//     $('.sns_popup2').show();
+// })
+// $('.sns3').on('click',function(e){
+//     e.preventDefault();
+//     $('.sns_popup3').show();
+// })
+// $('.sns4').on('click',function(e){
+//     e.preventDefault();
+//     $('.sns_popup4').show();
+// })
+// $('.sns5').on('click',function(e){
+//     e.preventDefault();
+//     $('.sns_popup5').show();
+// })
+// $('.sns6').on('click',function(e){
+//     e.preventDefault();
+//     $('.sns_popup6').show();
+// })
+// $('.sns7').on('click',function(e){
+//     e.preventDefault();
+//     $('.sns_popup7').show();
+// })
+// $('.sns8').on('click',function(e){
+//     e.preventDefault();
+//     $('.sns_popup8').show();
+// })
+
+// // 클로즈버튼 누르면 팝업닫기
+// $('.sns_close1').on('click',function(){
+//     $('.sns_popup1').hide();
+// })
+// $('.sns_close2').on('click',function(){
+//     $('.sns_popup2').hide();
+// })
+// $('.sns_close3').on('click',function(){
+//     $('.sns_popup3').hide();
+// })
+// $('.sns_close4').on('click',function(){
+//     $('.sns_popup4').hide();
+// })
+// $('.sns_close5').on('click',function(){
+//     $('.sns_popup5').hide();
+// })
+// $('.sns_close6').on('click',function(){
+//     $('.sns_popup6').hide();
+// })
+// $('.sns_close7').on('click',function(){
+//     $('.sns_popup7').hide();
+// })
+// $('.sns_close8').on('click',function(){
+//     $('.sns_popup8').hide();
+// })
 
 
 // 헤더메뉴 마우스 스크롤시 블랙적용
@@ -135,21 +163,32 @@ $(window).scroll(function() {
 
 
 // 스크롤시 풀페이지
-var mHtml = $("html");
-var page = 1;
+// var mHtml = $("html");
+// var page = 1;
 
 
-mHtml.animate({scrollTop : 0},10);
+// mHtml.animate({scrollTop : 0},10);
 
-$(window).on("wheel", function(e) {
-    if(mHtml.is(":animated")) return;
-    if(e.originalEvent.deltaY > 0) {
-        if(page == 10) return;
-        page++;
-    } else if(e.originalEvent.deltaY < 0) {
-        if(page == 1) return;
-        page--;
-    }
-    var posTop =(page-1) * $(window).height();
-    mHtml.animate({scrollTop : posTop});
+// $(window).on("wheel", function(e) {
+//     if(mHtml.is(":animated")) return;
+//     if(e.originalEvent.deltaY > 0) {
+//         if(page == 10) return;
+//         page++;
+//     } else if(e.originalEvent.deltaY < 0) {
+//         if(page == 1) return;
+//         page--;
+//     }
+//     var posTop =(page-1) * $(window).height();
+//     mHtml.animate({scrollTop : posTop});
+// })
+
+$('#wrap').fullpage({
+    scrollBar:true, //스크롤바 표시유무 true/flase
+    scrollingSpeed:700, //스크롤 속도
+    // menu:'.gnb',
+    // anchors:['home','profile','project_web1','project_landing1','project_design_detail'],
+    
+    navigation:true, //우측 네비게이션 유무
+    navigationTooltips:['Home','Profile','Skills','Progect-Web1', 'Progect-Web2', 'Landing1', 'Landing2', 'Design-Detail', 'Design-sns', 'end'],
+    showActiveTooltip:true,
 })
